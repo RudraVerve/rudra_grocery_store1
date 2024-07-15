b1f4fa87bb235137fc8a2cc7b574d2f62746b98b
RudraVerve <rudra.verve@gmail.com>
2024-07-15 18:22:30 +0530

    update on 15 jul

---
+++ lib/my_account/my_account_page.dart
@@ -0,0 +1,389 @@
+import 'package:flutter/material.dart';
+import '../task8_helper/login_page.dart';
+
+class MyAccount extends StatefulWidget {
+  final String additionalString;
+
+  MyAccount({super.key, this.additionalString = ''});
+
+  @override
+  _MyAccountState createState() => _MyAccountState();
+}
+
+class _MyAccountState extends State<MyAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.additionalString),
+        backgroundColor: Colors.teal[100],
+        foregroundColor: Colors.black,
+        automaticallyImplyLeading: false, // Removes the back button
+      ),
+      backgroundColor: Colors.teal[100],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 150,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 50,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Colors.black, // Border color
+                              width: 1.0,         // Border width
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Icon(Icons.shopping_bag, color: Colors.blue),
+                              Text('Orders',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'PlaywriteMX'),)
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 50,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Colors.black, // Border color
+                              width: 1.0,         // Border width
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Icon(Icons.favorite, color: Colors.red),
+                              Text('WishList',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'PlaywriteMX'),)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 50,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Colors.black, // Border color
+                              width: 1.0,         // Border width
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Icon(Icons.card_giftcard, color: Colors.redAccent),
+                              Text('Coupons',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'PlaywriteMX'),)
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 50,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Colors.black, // Border color
+                              width: 1.0,         // Border width
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Icon(Icons.call, color: Colors.blue),
+                              Text('Help Center',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'PlaywriteMX'),)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Align(alignment: Alignment.topLeft,child: Text('Acount Setting',style: TextStyle(fontFamily: 'LibreBaskerville', fontWeight: FontWeight.bold,fontSize: 20),)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.offline_bolt),
+                        SizedBox(width: 15),
+                        Text('Next Gen Plus',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),)
+                      ],
+                    ),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.person),
+                        SizedBox(width: 15),
+                        Text('Edit Account',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),)
+                      ],
+                    ),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.account_balance_wallet),
+                        SizedBox(width: 15),
+                        Text('Saved Card & Wallet',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),)
+                      ],
+                    ),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.location_on),
+                        SizedBox(width: 15),
+                        Text('Save Acount',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),)
+                      ],
+                    ),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.language),
+                        SizedBox(width: 15),
+                        Text('Select Language',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),)
+                      ],
+                    ),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.notifications_active),
+                        SizedBox(width: 15),
+                        Text('Notification Setting',style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),)
+                      ],
+                    ),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Colors.black, // Border color
+                          width: 1.0,         // Border width
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('Refer & Earn',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.share))
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Colors.black, // Border color
+                          width: 1.0,         // Border width
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('Earn Coins',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.currency_bitcoin_sharp))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Login(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.cyan,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.black, // Border color
+                      width: 1.0,         // Border width
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text('Log Out',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: 'LibreBaskerville'),),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),// Empty body
+      ),
+    );
+  }
+}
